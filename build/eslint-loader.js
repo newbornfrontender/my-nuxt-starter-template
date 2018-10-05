e284dea7c931bbc26569a78b5dcef0b8fabcc053
Infant Frontender <newbornfrontender@gmail.com>
2018-10-05 21:04:55 +0300

    Настроил eslint-loader

---
+++ build/eslint-loader.js
@@ -0,0 +1,12 @@
+export default async function () {
+  this.extendBuild((config, ctx) => {
+    if (ctx.isDev && ctx.isClient) {
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(jsx?|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      });
+    };
+  });
+};
