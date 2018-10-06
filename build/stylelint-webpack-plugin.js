72500f398ec842ba077516a92e11f64663a50db1
Infant Frontender <newbornfrontender@gmail.com>
2018-10-06 17:56:56 +0300

    Настроил stylelint-webpack-plugin модуль

---
+++ build/stylelint-webpack-plugin.js
@@ -0,0 +1,9 @@
+import StylelintWebpackPlugin from 'stylelint-webpack-plugin';
+
+export default async function () {
+  this.extendBuild(config => {
+    config.plugins.push(
+      new StylelintWebpackPlugin()
+    );
+  });
+};
