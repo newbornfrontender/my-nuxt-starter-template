93384957fb71ddf6d58b2878e43d36d07ac68aa5
Infant Frontender <newbornfrontender@gmail.com>
2018-10-07 21:45:15 +0300

    Убрал отступ

--- test/unit/pages/index.spec.js
+++ test/unit/pages/index.spec.js
@@ -4,7 +4,6 @@ import Component from '@/pages/index.vue';
 describe('Страница index', () => {
   test('Корректно отображается заголовок', () => {
     const wrapper = shallowMount(Component);
-
     expect(wrapper.find('h1').text()).toBe('Главная страница');
   });
 });
