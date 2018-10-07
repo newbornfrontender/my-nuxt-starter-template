dc38eec289b47bfc8834a6b626b7deb236786984
Infant Frontender <newbornfrontender@gmail.com>
2018-10-07 20:12:07 +0300

    Настроил плагин

--- build/stylelint-webpack-plugin.js
+++ build/stylelint-webpack-plugin.js
@@ -3,7 +3,9 @@ import StylelintWebpackPlugin from 'stylelint-webpack-plugin';
 export default async function () {
   this.extendBuild(config => {
     config.plugins.push(
-      new StylelintWebpackPlugin()
+      new StylelintWebpackPlugin({
+        files: '**/*.(css|vue)',
+      }),
     );
   });
 };
