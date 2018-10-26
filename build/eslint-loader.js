0d035b1806e9e03ab2b1f03d4a3881398c6f0d28
Infant Frontender <newbornfrontender@gmail.com>
2018-10-26 12:52:55 +0300

    Заменил ctx

--- build/eslint-loader.js
+++ build/eslint-loader.js
@@ -1,6 +1,6 @@
 export default async function() {
   // Можно использовать ctx 2 аргументом или {isDev, isClient}
-  this.extendBuild((config, {isDev, isClient}) => {
+  this.extendBuild((config, { isDev, isClient }) => {
     if (isDev && isClient) {
       config.module.rules.push({
         enforce: 'pre',
--- build/imagemin-webpack-plugin.js
+++ build/imagemin-webpack-plugin.js
@@ -1,8 +1,8 @@
 import ImageminPlugin from 'imagemin-webpack-plugin';
 
 export default async function() {
-  this.extendBuild((config, ctx) => {
-    if (!ctx.isDev) {
+  this.extendBuild((config, { isDev }) => {
+    if (!isDev) {
       config.plugins.push(
         new ImageminPlugin({
           test: /\.(jpe?g|png|gif|svg)$/i,
